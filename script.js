aa45cbf697e0af6305d5edf5011d0d5fcdaf7578
sunnypaul2390-eng <sunnypaul2390@gmail.com>
2026-03-18 21:42:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-if (username === "CSCSUNNY" && password === "12345") {
-  localStorage.setItem("loggedIn", "true");
-  window.location.href = "admin.html";
+function login() {
+  let username = document.getElementById("username").value.trim();
+  let password = document.getElementById("password").value.trim();
+
+  if (username === "CSCSUNNY" && password === "12345") {
+    localStorage.setItem("loggedIn", "true");
+    alert("Login Successful!");
+    window.location.href = "admin.html";
+  } else {
+    alert("Invalid Username or Password");
+  }
 }
